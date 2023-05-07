31098b0cc5baef6ed8a39d6f5506aa7cbb4a6997
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-05-06 21:58:48 -0300

    feat: ajuste de recurso de form style

--- src/presentation/components/FormGroup.js
+++ src/presentation/components/FormGroup.js
@@ -4,7 +4,7 @@ class FormGroup extends React.Component{
 
   render() {
     return(
-      <div className="form-group">
+      <div className="form-group" style={{marginBottom: '20px'}}>
         <label htmlFor={this.props.htmlFor}>{this.props.label}</label>
         {this.props.children}
       </div>
