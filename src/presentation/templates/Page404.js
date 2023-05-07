24c4172947723665145c57a5dc49bea447b07a3c
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-05-07 09:28:20 -0300

    feat: add page de erro

---
+++ src/presentation/templates/Page404.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Page404 extends React.Component {
+
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+            <div className='bs-docs-section'>
+             <div>Página Não Encontrada Erro 404</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Page404;
