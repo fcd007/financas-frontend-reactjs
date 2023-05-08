354a262ed0f50437d780600d93cf3f751514cd82
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-05-08 06:45:04 -0300

    feat: ajuste de componentes formulario

--- src/presentation/pages/CadastroUsuario.js
+++ src/presentation/pages/CadastroUsuario.js
@@ -1,17 +1,110 @@
 import React from "react";
 import Card from "../components/Card";
-import FormularioCadastroUsuario from "../components/FormularioCadastroUsuario";
+import FormGroup from "../components/FormGroup";
+import Container from "../components/Container";
 
-class CadastroUsuario extends React.Component{
+class CadastroUsuario extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleOnClick = this.handleOnClick.bind(this);
+    this.onChangeInput = this.onChangeInput.bind(this);
+
+    this.state = {
+      nome: "",
+      email: "",
+      password: "",
+      repeat: "",
+    };
+  }
+
+  onChangeInput = (event) => {
+    let valor = event.target.value;
+    let id = event.target.id;
+
+    this.setState({ [id]: valor });
+  };
+
+  handleOnClick(event) {
+    console.log("====================================");
+    console.log(this.state);
+    console.log("====================================");
+  }
 
   render() {
     return (
       <div className="container">
-      <Card title="Cadastro de Usuário" >
-        <FormularioCadastroUsuario />
-      </Card>
+        <Card title="Cadastro de Usuário">
+          <Container>
+            <FormGroup htmlFor="nome" label="Nome: *">
+              <input
+                style={{ paddingTop: "5px" }}
+                type="text"
+                className="form-control"
+                id="nome"
+                name="nome"
+                aria-describedby="text"
+                placeholder="Digite seu nome completo"
+                value={this.state.nome}
+                onChange={(event) => this.onChangeInput(event)}
+              />
+            </FormGroup>
+            <FormGroup htmlFor="email" label="Email: *">
+              <input
+                style={{ paddingTop: "5px" }}
+                type="email"
+                className="form-control"
+                id="email"
+                aria-describedby="email"
+                placeholder="Digite o e-mail"
+                value={this.state.email}
+                onChange={(event) => this.onChangeInput(event)}
+              />
+            </FormGroup>
+            <FormGroup htmlFor="password" label="Senha: *">
+              <input
+                style={{ paddingTop: "5px" }}
+                type="password"
+                className="form-control"
+                id="password"
+                aria-describedby="password"
+                placeholder="Digite sua senha"
+                value={this.state.password}
+                onChange={(event) => this.onChangeInput(event)}
+              />
+            </FormGroup>
+            <FormGroup htmlFor="password" label="Repetir a Senha: *">
+              <input
+                style={{ paddingTop: "5px" }}
+                type="password"
+                className="form-control"
+                id="repeat"
+                name="repeat"
+                aria-describedby="repeat"
+                placeholder="Digite repita sua senha"
+                value={this.state.repeat}
+                onChange={(event) => this.onChangeInput(event)}
+              />
+            </FormGroup>
+            <div style={{ padding: "10px" }}>
+              <button
+                className="btn btn-success"
+                onClick={(event) => this.handleOnClick(event)}
+              >
+                Salvar
+              </button>
+              <button
+                className="btn btn-danger"
+                style={{ marginLeft: "20px" }}
+                onClick={(event) => this.handleOnClick(event)}
+              >
+                Cancelar
+              </button>
+            </div>
+          </Container>
+        </Card>
       </div>
-    )
+    );
   }
 }
 
--- src/presentation/pages/Login.js
+++ src/presentation/pages/Login.js
@@ -1,23 +1,93 @@
-import React from 'react';
-import Card from '../components/Card';
-import FormularioLogin from '../components/FormularioLogin';
+import React from "react";
+import Card from "../components/Card";
+import FormGroup from "../components/FormGroup";
+import Container from "../components/Container";
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleOnClick = this.handleOnClick.bind(this);
+    this.onChangeInput = this.onChangeInput.bind(this);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleOnClick(event) {
+    console.log('====================================');
+    console.log(this.state);
+    console.log('====================================');
+  }
+
+  onChangeInput = (event) => {
+    let valor = event.target.value;
+    let id = event.target.id;
+      
+    this.setState({ [id]: valor });
+  };
 
   render() {
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-6' style={{position: 'relative', left: '300px'}}>
-            <div className='bs-docs-section'>
+      <div className="container">
+        <div className="row">
+          <div
+            className="col-md-6"
+            style={{ position: "relative", left: "300px" }}
+          >
+            <div className="bs-docs-section">
               <Card title="Login">
-                <FormularioLogin />
+                <Container>
+                  <FormGroup htmlFor="email" label="Email: *">
+                    <input
+                      style={{ paddingTop: "5px" }}
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      name="email"
+                      aria-describedby="email"
+                      placeholder="Digite o e-mail"
+                      value={this.state.email}
+                      onChange={(event) => this.onChangeInput(event)}
+                    />
+                  </FormGroup>
+                  <FormGroup htmlFor="password" label="Senha: *">
+                    <input
+                      style={{ paddingTop: "5px" }}
+                      type="password"
+                      className="form-control"
+                      id="password"
+                      name="password"
+                      aria-describedby="password"
+                      placeholder="Digite sua senha"
+                      value={this.state.password}
+                      onChange={(event) => this.onChangeInput(event)}
+                    />
+                  </FormGroup>
+                  <div style={{ padding: "10px" }}>
+                    <button
+                      className="btn btn-success"
+                      onClick={(event) => this.handleOnClick(event)}
+                    >
+                      Entrar
+                    </button>
+                    <button
+                      className="btn btn-danger"
+                      style={{ marginLeft: "20px" }}
+                      onClick={(event) => this.handleOnClick(event)}
+                    >
+                      Cadastrar
+                    </button>
+                  </div>
+                </Container>
               </Card>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
