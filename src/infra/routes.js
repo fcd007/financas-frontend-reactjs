9cd727b69993b40b3fa1aef69e2569ac7b84757b
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-05-11 21:41:58 -0300

    feat: ajuste de links e rotas

--- src/infra/routes.js
+++ src/infra/routes.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Page404 from "../presentation/templates/Page404";
 import Login from "../presentation/pages/Login";
 import CadastroUsuario from "../presentation/pages/CadastroUsuario";
 
 function Rotas() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro-usuarios" element={<CadastroUsuario />} />
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route index path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro-usuarios" element={<CadastroUsuario />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
   );
 }
 
--- src/main/App.js
+++ src/main/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "bootswatch/dist/materia/bootstrap.css";
-import "../styles/theme.css";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "../presentation/components/Navbar";
-
 import Rotas from "../infra/routes";
+import "../styles/theme.css";
 
 class App extends React.Component {
   render() {
     return (
-      <>
+      <BrowserRouter>
         <Navbar />
         <div className="container">
           <Rotas />
         </div>
-      </>
+      </BrowserRouter>
     );
   }
 }
