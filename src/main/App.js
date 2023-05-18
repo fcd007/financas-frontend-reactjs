4ae0ad0976b08101a9503486b03d6dbba26b63c8
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-05-17 21:20:39 -0300

    feat: add localstorage login

--- src/main/App.js
+++ src/main/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "bootswatch/dist/materia/bootstrap.css";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "../presentation/components/Navbar";
-import Rotas from "../infra/routes";
+import Rotas from "../infra/routes/routes";
 import "../styles/theme.css";
 
 class App extends React.Component {
