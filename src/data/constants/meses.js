dd13d8f6edf52f7f0f766e299e3b9e3579e8e46b
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-06-02 06:25:35 -0300

    feat: add constante meses

---
+++ src/data/constants/meses.js
@@ -0,0 +1,16 @@
+const MESES_ANO = [
+  { id: 0, descricao: "Janeiro" },
+  { id: 1, descricao: "Fevereiro" },
+  { id: 2, descricao: "Março" },
+  { id: 3, descricao: "Abril" },
+  { id: 4, descricao: "Maio" },
+  { id: 5, descricao: "Junho" },
+  { id: 6, descricao: "Julho" },
+  { id: 7, descricao: "Agosto" },
+  { id: 8, descricao: "Setembro" },
+  { id: 9, descricao: "Outubro" },
+  { id: 10, descricao: "Novembro" },
+  { id: 11, descricao: "Dezembro" },
+];
+
+export default MESES_ANO;
