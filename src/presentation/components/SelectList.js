47a51e575bc71080056985c68ed29419243ef8e0
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-06-02 06:25:52 -0300

    feat: add componente select list  meses

---
+++ src/presentation/components/SelectList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+
+
+export default function SelectList(props) {
+  const options = props.lista.map(option => {
+    return (
+      <option key={option.id} value={option.id}>{option.descricao}</option>
+    )
+  })
+  
+  return (
+    <Form.Select>
+      {options}
+    </Form.Select>
+  )
+}
